dbed4229a6e182371ddbda9d4e71b1472792c71e
omeru <omeruncu@outlook.com>
2025-06-28 19:46:09 +0300

    Encoding Scaling added

---
+++ encoding_scaling.py
@@ -0,0 +1,294 @@
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler, RobustScaler
+
+# Set display options for pandas
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+def load_application_train():
+    data = pd.read_csv("data/application_train.csv")
+    return data
+
+
+def load():
+    data = pd.read_csv("data/titanic.csv")
+    return data
+
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Gives the names of categorical, numeric and categorical but cardinal variables in the data set.
+    Note: Categorical variables with numeric appearance are also included in categorical variables.
+
+    Parameters
+    ------
+        dataframe: dataframe
+            Dataframe from which variable names are to be taken
+        cat_th: int, optional
+            Class threshold value for numeric but categorical variables
+        car_th: int, optional
+            Class threshold value for categorical but cardinal variables    
+
+    Returns
+    ------
+        cat_cols: list
+            Categorical variable list
+        num_cols: list
+            Numerical variable list
+        cat_but_car: list
+            Cardinal variable list with categorical view
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = total number of variables
+        num_but_cat is inside cat_cols.
+        The total of the 3 lists that are returned is equal to the total number of variables: cat_cols + num_cols + cat_but_car = number of variables
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+
+#############################################
+# Encoding (Label Encoding, One-Hot Encoding, Rare Encoding)
+#############################################
+
+#############################################
+# Label Encoding & Binary Encoding
+#############################################
+
+df = load()
+df.head()
+df["Sex"].head()
+
+le = LabelEncoder()
+le.fit_transform(df["Sex"])[0:5]
+le.inverse_transform([0, 1])
+
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+df = load()
+
+binary_cols = [col for col in df.columns if df[col].dtype not in [int, float]
+               and df[col].nunique() == 2]
+
+for col in binary_cols:
+    label_encoder(df, col)
+
+df.head()
+
+df = load_application_train()
+df.shape
+
+binary_cols = [col for col in df.columns if df[col].dtype not in [int, float]
+               and df[col].nunique() == 2]
+
+df[binary_cols].head()
+
+
+for col in binary_cols:
+    label_encoder(df, col)
+
+
+df = load()
+df["Embarked"].value_counts()
+df["Embarked"].nunique()
+len(df["Embarked"].unique())
+
+#############################################
+# One-Hot Encoding
+#############################################
+
+df = load()
+df.head()
+df["Embarked"].value_counts()
+
+pd.get_dummies(df, columns=["Embarked"]).head()
+
+pd.get_dummies(df, columns=["Embarked"], drop_first=True).head()
+
+pd.get_dummies(df, columns=["Embarked"], dummy_na=True).head()
+
+pd.get_dummies(df, columns=["Sex", "Embarked"], drop_first=True).head()
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=True):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+df = load()
+
+# cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+ohe_cols = [col for col in df.columns if 10 >= df[col].nunique() > 2]
+
+
+one_hot_encoder(df, ohe_cols).head()
+
+df.head()
+
+#############################################
+# Rare Encoding
+#############################################
+
+# 1. Analyze the rarity of categorical variables.
+# 2. Analyze the relationship between rare categories and dependent variables.
+# 3. We will write a rare encoder.
+
+###################
+# 1. Analyze the rarity of categorical variables.
+###################
+
+df = load_application_train()
+df["NAME_EDUCATION_TYPE"].value_counts()
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+
+for col in cat_cols:
+    cat_summary(df, col)
+
+###################
+# 2. Analyze the relationship between rare categories and dependent variables.
+###################
+
+df["NAME_INCOME_TYPE"].value_counts()
+
+df.groupby("NAME_INCOME_TYPE")["TARGET"].mean()
+
+
+def rare_analyser(dataframe, target, cat_cols):
+    for col in cat_cols:
+        print(col, ":", len(dataframe[col].value_counts()))
+        print(pd.DataFrame({"COUNT": dataframe[col].value_counts(),
+                            "RATIO": dataframe[col].value_counts() / len(dataframe),
+                            "TARGET_MEAN": dataframe.groupby(col)[target].mean()}), end="\n\n\n")
+
+rare_analyser(df, "TARGET", cat_cols)
+
+#############################################
+# 3. Writing the Rare Encoder.
+#############################################
+
+def rare_encoder(dataframe, rare_perc):
+    temp_df = dataframe.copy()
+
+    rare_columns = [col for col in temp_df.columns if temp_df[col].dtypes == 'O'
+                    and (temp_df[col].value_counts() / len(temp_df) < rare_perc).any(axis=None)]
+
+    for var in rare_columns:
+        tmp = temp_df[var].value_counts() / len(temp_df)
+        rare_labels = tmp[tmp < rare_perc].index
+        temp_df[var] = np.where(temp_df[var].isin(rare_labels), 'Rare', temp_df[var])
+
+    return temp_df
+
+new_df = rare_encoder(df, 0.01)
+
+new_df.head()
+
+rare_analyser(new_df, "TARGET", cat_cols)
+
+df["OCCUPATION_TYPE"].value_counts()
+
+
+#############################################
+# Feature Scaling
+#############################################
+
+###################
+# StandardScaler: Classic standardization. Subtract the mean, divide by the standard deviation. z = (x - u) / s
+###################
+
+df = load()
+ss = StandardScaler()
+df["Age_standard_scaler"] = ss.fit_transform(df[["Age"]])
+df.head()
+
+
+###################
+# RobustScaler: Subtract the median, divide by the IQR.
+###################
+
+rs = RobustScaler()
+df["Age_robust_scaler"] = rs.fit_transform(df[["Age"]])
+df.describe().T
+
+###################
+# MinMaxScaler: Variable conversion between 2 given values (default 0-1). x_scaled = (x - min) / (max - min)
+###################
+
+# X_std = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
+# X_scaled = X_std * (max - min) + min
+
+mms = MinMaxScaler()
+df["Age_min_max_scaler"] = mms.fit_transform(df[["Age"]])
+df.describe().T
+
+df.head()
+
+age_cols = [col for col in df.columns if "Age" in col]
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+for col in age_cols:
+    num_summary(df, col, plot=True)
+
+###################
+# Numeric to Categorical: Converting Numerical Variables to Categorical Variables
+# Binning
+###################
+
+df["Age_qcut"] = pd.qcut(df['Age'], 5)
